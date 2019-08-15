bcb733805aab789d63419d2c11fa82ad0eca1987
Amanda Garcia <algarcian@outlook.com>
2019-08-15 12:47:46 -0500

    complete input render tests and code

--- src/components/Input.js
+++ src/components/Input.js
@@ -2,17 +2,26 @@ import React, { Component } from 'react';
 import {connect} from 'react-redux'; 
 
 class Input extends Component {
+
   render() {
+    const contents = this.props.success ? null : (
+      <form className="form-inline">
+        <input id="word-guess" type="text" placeholder="enter guess" data-test="input-box" className="mb-2 mx-sm-3"/>
+        <button data-test="submit-button" type="submit" className="btn btn-primary mb-2">submit</button>
+      </form>
+    )
+
+    
     return (
-      <div>
-        <button></button>
+      <div data-test="component-input">
+        {contents}
       </div>
     )
   }
 }
 
-const mapStateToProps = (state) => {
-  return {}; 
+const mapStateToProps = ({success}) => {
+  return {success}; 
 }
 
 export default connect(mapStateToProps)(Input); 
