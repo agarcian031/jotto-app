2aa7c0de915182fc5e0fcba821915db70a05f28a
Amanda Garcia <algarcian@outlook.com>
2019-08-16 15:12:16 -0500

    added tests for redux store

---
+++ src/helpers/index.js
@@ -0,0 +1,5 @@
+export function getLetterMatchCount(guessedWord, secretWord) {
+  const secretLetterSet = new Set(secretWord.split('')); 
+  const guessedLetterSet = new Set(guessedWord.split('')); 
+
+}
