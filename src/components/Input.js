dafb65e4cb318bd0bf8fafb059a9a5a9342165c4
Amanda Garcia <algarcian@outlook.com>
2019-08-20 09:26:34 -0500

    write guessWord call with arugment

--- src/components/Input.js
+++ src/components/Input.js
@@ -6,14 +6,24 @@ export class UnconnectedInput extends Component {
   constructor(props){
     super(props); 
     this.inputBox = React.createRef(); 
+    // this.submitGuessedWord = this.submitGuessedWord.bind(this)
 
+  }
+
+  submitGuessedWord = (event) => {
+    // prevent default 
+    event.preventDefault(); 
+    const guessedWord = this.inputBox.current.value; 
+    if(guessedWord && guessedWord.length > 0){
+      this.props.guessWord(guessedWord); 
+    }
   }
 
   render() {
     const contents = this.props.success ||this.props.gaveUp ? null : (
       <form className="form-inline">
         <input id="word-guess" type="text" placeholder="enter guess" data-test="input-box" ref={this.inputBox} className="mb-2 mx-sm-3"/>
-        <button data-test="submit-button" type="submit" className="btn btn-primary mb-2" onClick={() => this.props.guessWord('train')}>submit</button>
+        <button data-test="submit-button" type="submit" className="btn btn-primary mb-2" onClick={this.submitGuessedWord}>submit</button>
       </form>
     )
 
